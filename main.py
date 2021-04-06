03f81c7887980c986a16c8e2773d4c3d308cb951
William <williamjbf@gmail.com>
2021-04-06 18:29:38 -0300

    exclusao e busca de contatos

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from contato import Contato
 
 print("-" * 30)
-print(("-" *3) + " Agenda de contatos " + ("-" *3))
+print(("-" * 3) + " Agenda de contatos " + ("-" * 3))
 print("-" * 30)
 
 opcaoMenu = 1
 listaContatos = list()
 
-while(opcaoMenu != 0):
+while (opcaoMenu != 0):
     print("1. Listar contatos")
     print("2. Cadastrar contato")
+    print("3. Remover contato")
+    print("4. Buscar contato")
     print("0. Sair")
     opcaoMenu = int(input("Digite a opção desejada: "))
 
@@ -24,7 +26,27 @@ while(opcaoMenu != 0):
         novoContato = Contato(nomeContato, emailContato, telefoneContato)
         listaContatos.append(novoContato)
 
+    elif opcaoMenu == 3:
+        contatoARemover = input("Digite o email do contato que deseja remover: ")
+        contatoEncontrado = False
+        for i in listaContatos:
+            if i.email == contatoARemover:
+                listaContatos.remove(i)
+                print("Contato removido")
+                contatoEncontrado = True
+                continue
+        if not contatoEncontrado:
+            print("Contato não encontrado")
+
+    elif opcaoMenu == 4:
+        buscarContato = input("Digite o email do contato que deseja buscar: ")
+        contatoEncontrado = False
+        for i in listaContatos:
+            if i.email == buscarContato:
+                print(f"nome: {i.nome} / email: {i.email} / telefone: {i.telefone}")
+                contatoEncontrado = True
+                continue
     else:
         print("opcao invalida")
 else:
-    print("Obrigado por usar a agenda de contatos TW")
+    print("Obrigado por usar a agenda de contatos")
